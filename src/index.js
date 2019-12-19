62d10709a828a0c0e010808167c9ae1f55632741
augustoberwaldt <augusto.berwaldt@gmail.com>
2019-12-19 16:37:32 -0300

    FEA:Adiciona dashboard

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import SignInSide from './components/login/SignInSide'
+import Dashboard from './components/dashboard/Dashboard'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter,Switch, Route } from 'react-router-dom'
 
-ReactDOM.render(<SignInSide />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(  
+<BrowserRouter> 
+<Switch>
+    <Route path="/" exact={true} component={SignInSide} />
+    <Route path="/home"  component={Dashboard} />
+</Switch>
+
+</BrowserRouter>, document.getElementById('root'));
+
 serviceWorker.unregister();
